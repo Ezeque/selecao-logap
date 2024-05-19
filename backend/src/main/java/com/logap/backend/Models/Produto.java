1e19520f1b2228dfb4ef864eae31789308268447
Ezequiel Morais <ezequielmorais4002@gmail.com>
2024-05-19 08:40:22 -0300

    criados modelos da gestão de estoque

---
+++ backend/src/main/java/com/logap/backend/Models/Produto.java
@@ -0,0 +1,31 @@
+package com.logap.backend.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private float valor;
+    private int quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "id_fornecedor")
+    private Fornecedor fornecedor;
+
+}
