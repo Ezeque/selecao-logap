2e321ce3e10ce8ec718c5b4090044e0db6680787
Ezequiel Morais <ezequielmorais4002@gmail.com>
2024-05-20 09:40:37 -0300

    criada tela de seleção de módulo

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <VBtn v-if="$route.path != '/'" id="go-back" icon="mdi-arrow-left" color="#5C4BAA" @click="$router.back()">
+        
+      </VBtn>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import SelecaoModulo from './components/Utils/SelecaoModulo.vue'
+</script>
+<style scoped>
+#go-back{
+ float: left;
+ margin: 1em; 
+}
+</style>
---
+++ frontend/src/components/Utils/SelecaoModulo.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { defineProps } from 'vue';
+import { useRouter } from 'vue-router';
+import { VIcon } from 'vuetify/components';
+const props = defineProps({
+    icon: String,
+    text: String,
+    route: {type: String, required: true}
+})
+
+const router = useRouter()
+
+const gotToModule = () => {
+    router.push(props.route)
+}
+
+
+</script>
+
+<template>
+    <div id="botao" @click="gotToModule">
+        <VIcon size="50" :icon="icon" />
+        <span>{{text}}</span>
+    </div>
+</template>
+
+<style scoped>
+#botao {
+    height: 8em;
+    width: 8em;
+    border: 1px solid #5C4BAA;
+    color: #5C4BAA;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    transition: 0.3s all ease;
+    font-weight: bold;
+    text-align: center;
+    padding: 0;
+}
+
+#botao:hover {
+    background-color: #5C4BAA;
+    color: white;
+    cursor: pointer;
+    scale: 120%;
+}
+</style>
