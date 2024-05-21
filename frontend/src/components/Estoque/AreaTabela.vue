da7f1f666f1faecd742dc35a8403e13aa2bfd1aa
Ezequiel Morais <ezequielmorais4002@gmail.com>
2024-05-21 10:10:22 -0300

    Adicionada validação ao formulário dew criação de produtos

--- frontend/src/components/Estoque/AreaTabela.vue
+++ frontend/src/components/Estoque/AreaTabela.vue
@@ -11,15 +11,22 @@ const erro: Ref<boolean> = ref(false)
 const mostrarNovoProduto: Ref<boolean> = ref(false)
 const categoriaAutocomplete: Ref<any> = ref({ filteredItems: [] })
 const fornecedorAutocomplete: Ref<any> = ref({ filteredItems: [] })
+const formValid:Ref<boolean> = ref()
+
+const rules = {
+    required: (valor: string) => !!valor || 'Campo Obrigatório'
+}
 
 const novoProduto: Ref<Produto> = ref(new Produto())
 const novaCategoria: Ref<Categoria> = ref(new Categoria())
 const novoFornecedor: Ref<Fornecedor> = ref(new Fornecedor())
 
 const salvarProduto = async () => {
-    mostrarNovoProduto.value = false
-    await criaProduto(novoProduto.value)
-    await recuperaTodosProdutos()
+    if (formValid.value) {
+        mostrarNovoProduto.value = false
+        await criaProduto(novoProduto.value)
+        await recuperaTodosProdutos()
+    }
 }
 
 const salvarCategoria = async (nomeCategoria: string) => {
@@ -125,44 +132,50 @@ onMounted(async () => {
 
         <VDialog fullscreen v-model="mostrarNovoProduto">
             <VCard>
-                <VToolbar class="toolbar text-white mb-5">
-                    <VBtn icon="mdi-close" @click="mostrarNovoProduto = false"></VBtn>
+                <VForm v-model="formValid" ref="form" @submit.prevent="salvarProduto">
+                    <VToolbar class="toolbar text-white mb-5">
+                        <VBtn icon="mdi-close" @click="mostrarNovoProduto = false"></VBtn>
 
-                    <VToolbarTitle>Cadastrar Novo Produto</VToolbarTitle>
+                        <VToolbarTitle>Cadastrar Novo Produto</VToolbarTitle>
 
-                    <VSpacer></VSpacer>
+                        <VSpacer></VSpacer>
 
-                    <VToolbarItems>
-                        <VBtn class="" @click="salvarProduto">Salvar Produto</VBtn>
-                    </VToolbarItems>
-                </VToolbar>
-                <VForm>
+                        <VToolbarItems>
+                            <VBtn type="submit"> Salvar Produto</VBtn>
+                        </VToolbarItems>
+                    </VToolbar>
+
                     <VRow>
                         <VCol>
-                            <VTextField label="Nome" placeholder="Nome do Produto" hide-details
+                            <VTextField :rules="[rules.required]" label="Nome" placeholder="Nome do Produto" hide-details="auto"
                                 v-model="novoProduto.name" />
                         </VCol>
                     </VRow>
                     <VRow>
                         <VCol md="6">
-                            <VAutocomplete ref="categoriaAutocomplete" label="Categoria"
+
+                            <VAutocomplete :rules="[rules.required]" ref="categoriaAutocomplete" label="Categoria"
                                 placeholder="Categoria do Produto" return-object :items="categorias" item-title="nome"
-                                v-model="novoProduto.categoria" >
+                                v-model="novoProduto.categoria"
+                                hide-details="auto">
+
                                 <template v-if="mostrarNovoProduto" #append-inner>
-                                    <VBtn @click="salvarCategoria(categoriaAutocomplete?.modelValue)" color="success" 
-                                    v-if="categoriaAutocomplete?.filteredItems.length == 0 && categoriaAutocomplete.modelValue != ''">
+                                    <VBtn @click="salvarCategoria(categoriaAutocomplete?.modelValue)" color="success"
+                                        v-if="categoriaAutocomplete?.filteredItems.length == 0 && categoriaAutocomplete.modelValue != ''">
                                         Criar Categoria {{ categoriaAutocomplete?.modelValue }}
                                     </VBtn>
                                 </template>
                             </VAutocomplete>
                         </VCol>
+
                         <VCol md="6">
-                            <VAutocomplete ref="fornecedorAutocomplete" label="Fornecedor"
-                                placeholder="Fornecedor do Produto" return-object :items="fornecedores" item-title="nome"
-                                v-model="novoProduto.fornecedor" >
+                            <VAutocomplete :rules="[rules.required]" ref="fornecedorAutocomplete" label="Fornecedor"
+                                placeholder="Fornecedor do Produto" return-object :items="fornecedores"
+                                item-title="nome" v-model="novoProduto.fornecedor"
+                                hide-details="auto">
                                 <template v-if="mostrarNovoProduto" #append-inner>
-                                    <VBtn @click="salvarFornecedor(fornecedorAutocomplete?.modelValue)" color="success" 
-                                    v-if="fornecedorAutocomplete?.filteredItems.length == 0 && fornecedorAutocomplete.modelValue != ''">
+                                    <VBtn @click="salvarFornecedor(fornecedorAutocomplete?.modelValue)" color="success"
+                                        v-if="fornecedorAutocomplete?.filteredItems.length == 0 && fornecedorAutocomplete.modelValue != ''">
                                         Criar Fornecedor {{ fornecedorAutocomplete?.modelValue }}
                                     </VBtn>
                                 </template>
@@ -171,16 +184,16 @@ onMounted(async () => {
                     </VRow>
                     <VRow>
                         <VCol md="6">
-                            <VTextField label="valor" placeholder="Valor do Produto" hide-details
+                            <VTextField :rules="[rules.required]" label="valor" placeholder="Valor do Produto" hide-details="auto"
                                 v-model="novoProduto.valor" />
                         </VCol>
                         <VCol md="6">
-                            <VTextField label="Quantidade" placeholder="Quantidade do Produto" hide-details
+                            <VTextField :rules="[rules.required]" label="Quantidade" placeholder="Quantidade do Produto" hide-details="auto"
                                 v-model="novoProduto.quantidade" />
                         </VCol>
                     </VRow>
                     <VRow>
-                        <VTextField label="Localização" placeholder="Localização do Produto" hide-details
+                        <VTextField label="Localização" placeholder="Localização do Produto" hide-details="auto"
                             v-model="novoProduto.localizacao" />
                     </VRow>
                 </VForm>
